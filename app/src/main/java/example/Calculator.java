20116a104878f5fbbb91ea5614239e8b5e3720a9
langanc <langanc@student.unimelb.edu.au>
2025-03-05 17:17:23 +1100

    implement add() and minus()

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,18 +2,16 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
-        return 4;
+        return a + b;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+            // TODO: Implement this method
+            return 10;
     }
 
     public double divide(int a, int b) {
